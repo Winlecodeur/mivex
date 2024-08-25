69f42a2fbcc41b8ac6e75f4e557e34904cbb7b8a
Winlecodeur <163679684+Winlecodeur@users.noreply.github.com>
2024-08-26 01:52:43 +0200

    Update settings.py

--- mivex/settings.py
+++ mivex/settings.py
@@ -122,7 +122,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES = [os.path.join(BASE_DIR,'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join('media')
-"whitenoise.storage.CompressedManifestStaticFilesStorage
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
