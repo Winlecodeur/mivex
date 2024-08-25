63947b43774a5f9709c313903741132f6d90e882
Winlecodeur <163679684+Winlecodeur@users.noreply.github.com>
2024-08-26 01:52:02 +0200

    Update settings.py

--- mivex/settings.py
+++ mivex/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c4%=+i!z5&ohn0#*r#+#^(9yt7xw4v2_35=w%wu83x#l1m1+c6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [mivex.onrender.com]
 
 
 # Application definition
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware', 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware', 
 ]
 
 ROOT_URLCONF = 'mivex.urls'
