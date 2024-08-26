e930b63a19ccd929efe264475730420d67c4ccce
Winlecodeur <163679684+Winlecodeur@users.noreply.github.com>
2024-08-26 12:03:07 +0200

    Update settings.py.

--- mivex/settings.py
+++ mivex/settings.py
@@ -128,8 +128,8 @@ MEDIA_ROOT = os.path.join('media')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'therryconsu@gmail.com'
-EMAIL_HOST_PASSWORD = 'therry2005'
+EMAIL_HOST_USER = 'smtp.mivex115@gmail.com'
+EMAIL_HOST_PASSWORD = 'smtp.mivex115@gmail.com'
 DEFAULT_FROM_EMAIL= 'therryconsu@gmail.com'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
