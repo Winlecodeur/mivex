2518a82ad602cca6ed83c63a7e73b40415978c9c
Winlecodeur <163679684+Winlecodeur@users.noreply.github.com>
2024-08-26 02:13:49 +0200

    Update settings.py

--- mivex/settings.py
+++ mivex/settings.py
@@ -117,11 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES = [os.path.join(BASE_DIR,'static')]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join('media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
