3fb7a7cf3d5a44a43f131e186b664c71c076a9e4
Winlecodeur <163679684+Winlecodeur@users.noreply.github.com>
2024-08-26 02:10:31 +0200

    Update settings.py

--- mivex/settings.py
+++ mivex/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES = [os.path.join(BASE_DIR,'static')]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join('media')
 
